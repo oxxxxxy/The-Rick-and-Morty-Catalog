9d595e2e88552e474db155553396af4acc73d837
oxxxxxy <dawpbbgdfrphm212@outlook.com>
2024-06-20 15:23:36 +0300

    working on the svelte-ts-graphql version #11; /characters; customForm; SelectMenu; wierd error... na vsyakiy sohranyus'...

--- packages/ts-LF-types/src/selectOption.ts
+++ packages/ts-LF-types/src/selectOption.ts
@@ -1,5 +1,15 @@
+import type { PositiveInteger } from '@tsL/types';
+
+
+
+
 export type SelectOption = {
 	value: string;
 	selected?: true;
 	default?: true;
+	id?: PositiveInteger<number> | 0;
+}
+
+export type IndexedSelectOption = SelectOption & {
+	id: PositiveInteger<number> | 0;
 }
--- v-svelte-ts-graphql/src/components/svelte/customForm/SelectMenu.svelte
+++ v-svelte-ts-graphql/src/components/svelte/customForm/SelectMenu.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { SelectOption } from '@tsLF/types';
+	import type { IndexedSelectOption } from '@tsLF/types';
 
 
 	import SelectMenuIcon from '$comps/svelte/customForm/icons/SelectMenuIcon.svelte';
@@ -8,11 +8,106 @@
 
 
 	//dev
+	import {
+		API_CHARACTERS__PARAM__STATUS
+	} from '@tsCF/data';
+	
+	import type {
+		QueryParamCompatible_Form_Selection
+	} from '@tsLF/types';
+	import type { PositiveInteger } from '@tsL/types';
+	
+	import { capitalizeWord } from '@tsCF/pages/src/index.ts';
+
+	const makeIndexedSelectOptionsFromQPCFormSelection = (
+		{	
+			objWithTypeOptions, 
+			doUNeedSetSelected, 
+			doUNeedDefaultNonValue
+		} : {
+			objWithTypeOptions: QueryParamCompatible_Form_Selection,
+			doUNeedSetSelected?: PositiveInteger<number> | 0 | string,
+			doUNeedDefaultNonValue?: true
+		}
+	): IndexedSelectOption[] | void => {
+
+		const makeIndexedSelectOption = (
+			id: PositiveInteger<number> | 0,
+			value: string,
+			default_?: true,
+		): IndexedSelectOption => {
+			const obj: IndexedSelectOption = {
+				id,
+				value
+			};
+
+			if(default_){
+				obj.default = default_;
+			}
+
+			return obj;
+		};
+
+
+		const resultArr: IndexedSelectOption[] = [];
+
+		if(doUNeedDefaultNonValue){
+			const res = makeIndexedSelectOption(
+				resultArr.length,
+				capitalizeWord(objWithTypeOptions.name),
+				true
+			);
+
+			resultArr.push(res);
+		}
+
+		
+		for(const option of objWithTypeOptions.options){
+			const res = makeIndexedSelectOption(
+				resultArr.length,
+				capitalizeWord(option),
+			);
+
+			resultArr.push(res);
+		}
+
+
+		if(doUNeedSetSelected){
+			if(typeof doUNeedSetSelected === 'number'){
+				resultArr[doUNeedSetSelected].selected = true;
+			} else if( typeof doUNeedSetSelected === 'string' ){
+				const index = resultArr.findIndex( e => e.value.toLowerCase() === doUNeedSetSelected.toLowerCase());
+
+				if(index >= 0){
+					resultArr[index].selected = true;
+				} else {
+					throw new Error(`doUNeedSetSelected string value "${doUNeedSetSelected}" does not exist in options of the "${objWithTypeOptions.name}".');
+				}
+			}
+		}
+
+
+		if(!resultArr.length){
+			throw new Error('resultArr is empty.');
+		}
+
+
+		return resultArr;
+	};
+
+
 
 
 	//dev
-	export const options: SelectOption[] = [];
-	export let selected: SelectOption;
+	const _opts = = makeIndexedSelectOption({
+		objWithTypeOptions: API_CHARACTERS__PARAM__STATUS,
+		doUNeedDefaultNonValue: true
+	});
+	export const options: IndexedSelectOption[] = _opts 
+
+
+	//export const options: IndexedSelectOption[] = [];
+	export let selected: IndexedSelectOption;
 	export let active: boolean = false;
 
 	$: _active = active;
@@ -28,17 +123,48 @@
 	};
 </script>
 
+
 <div
-  class="select-list-box select-list-box--statusGender"
+  class="
+		select-list-box
+		select-list-box--statusGender
+	"
   style:overflow="{ _active ? 'visible' : 'hidden' }"
 	on:click={useSelectMenu}
-	role="menu"
 >
   <div class="select-list bg-color--282828 d-flex fd-column">
-    <span class="select-list-option-border select-list-option selected-select-list-option"> Status</span>
-    <span class="select-list-option-border select-list-option"> alive</span>
-    <span class="select-list-option-border select-list-option"> dead</span>
-    <span class="select-list-option-border select-list-option select-list-option-border-last"> unknown</span>
+		{#each options as option, i }
+			{#if i === 0 }
+	   	 <span
+					class="
+						select-list-option-border
+						select-list-option
+						selected-select-list-option
+					"
+				>
+					{option.value}
+				</span>
+	    {:else if i === options.length - 1}
+				<span
+					class="
+						select-list-option-border
+						select-list-option
+						select-list-option-border-last
+					"
+				>
+					{option.value}
+				</span>
+			{:else}
+				<span 
+					class="
+						select-list-option-border
+						select-list-option
+					"
+				>
+					{option.value}
+				</span>
+	    {/if}
+    {/each}
   </div>
   <div class="d-flex ai-center jc-center color--282828 select-list-icon">
 		<SelectMenuIcon />
