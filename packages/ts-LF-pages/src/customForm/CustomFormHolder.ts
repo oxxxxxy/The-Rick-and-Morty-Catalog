38e520da7cb87bbb6720aaef89340e786ba29cb7
oxxxxxy <dawpbbgdfrphm212@outlook.com>
2024-06-27 23:41:02 +0300

    working on the svelte-ts-graphql version #29; /characters; CharactersSearch.svelte; customForm solution trip...

---
+++ packages/ts-LF-pages/src/customForm/CustomFormHolder.ts
@@ -0,0 +1,24 @@
+import type {
+	QPC_SelectOption,
+	QPC_InputText,
+	QueryParamCompatible_Base
+} from '@tsLF/forURLSP';
+
+import 
+
+
+
+
+//export type 
+
+export class CustomFormHolder{
+
+	constructor(
+		{
+			CFIDC_
+		} : 
+	){
+		
+	}
+
+}
