57fb420219304a6614582bb2134d51121dc5b93c
oxxxxxy <dawpbbgdfrphm212@outlook.com>
2024-07-21 20:43:49 +0300

    working on the svelte-ts-graphql version #54; /characters; connecting our window.location and CharactersSearch; sho, pitaemsa.; window.location tracking was made; urlsearchparams tracking making #3; green day)))

--- packages/ts-LF-wLocationChangeEvent/src/LocationSearchChangeEventEmitter.ts
+++ packages/ts-LF-wLocationChangeEvent/src/LocationSearchChangeEventEmitter.ts
@@ -15,8 +15,16 @@ export type ArgumentsFor_LocationSearchChangeEventEmitter = ArgumentsFor_Observa
 	pathname: string;
 };
 
+export type IndexedSearchParam = {
+	value: string;
+	id: number;
+};
+
 export class LocationSearchChangeEventEmitter extends Observable implements Observer{
 	readonly pathname: string;
+	#previousSearchParams: IndexedSearchParam[] = [];
+	#currentSearchParams: IndexedSearchParam | undefined;
+	
 
 	constructor(
 		{
@@ -26,12 +34,86 @@ export class LocationSearchChangeEventEmitter extends Observable implements Obse
 	){
 		super({observers});
 		
-		this.pathname = pathname;
+		const T = this;
+
+		T.pathname = pathname;
+
+		const interval = setInterval(
+			() => {
+				try{
+					T.#pushToPreviousSearchParams(window.location.search);
+					T.#setCurrentSearchParam();
+
+					clearInterval(interval);
+				} catch (e){
+					return;
+				}
+			}
+		);
+	}
+
+	#pushToPreviousSearchParams(searchParam: string | IndexedSearchParam){
+		const type = typeof searchParam;
+
+		if(type === 'object'){
+			searchParam = {...searchParam};
+
+			searchParam.id = this.#previousSearchParams.length;
+		}
+
+		searchParam = {
+			value: searchParam,
+			id: this.#previousSearchParams.length
+		}
+
+		this.#previousSearchParams.push(searchParam);
+	}
+
+	#setCurrentSearchParam(searchParam?: IndexedSearchParam){
+		switch(typeof searchParam){
+			case 'object':
+				searchParam = {...searchParam};
+				break;
+			case 'undefined':
+				searchParam = {...this.#previousSearchParams[this.#previousSearchParams.length - 1]}
+				break;
+		}
+
+		this.#currentSearchParams = searchParam;
+	}
+
+	#getEventData(): LocationSearchChangeEventData{
+		return {
+			pathname: this.pathname,
+			history: this.#previousSearchParams.map(e => ({...e})),
+			current: this.#currentSearchParams,
+			
+		}
 	}
 
 	onNotification(data: WindowLocationData){
+		if(data.pathname != this.pathname){
+			return;
+		}
+
+		const index = this.#previousSearchParams.findLastIndex((e: IndexedSearchParam) => e.value === data.search);
 
 
+		if(index < 0){//new
+
+			this.#pushToPreviousSearchParams(data.search);
+			this.#setCurrentSearchParam();
+		
+			const eventData = this.#getEventData();
+
+			this.notify(eventData);
+
+		} else if( ItWasCreatedByUserNotNavigated ){ //new
+
+		} else { //navigated
+
+			
+		}
 
 
 
