d69c60fe1e561ce1161a21d22802291485fa65b4
oxxxxxy <dawpbbgdfrphm212@outlook.com>
2024-08-26 21:31:46 +0300

    working on the svelte-ts-graphql version #79; svelte bug in file routing, idk what to do... and in this repo part i will do nothing with that; right now it's time to try vercel deployment;

--- v-svelte-ts-graphql/svelte.config.js
+++ v-svelte-ts-graphql/svelte.config.js
@@ -1,6 +1,10 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import path from 'node:path';
+
+
+import adapter_static from '@sveltejs/adapter-static';
+import adapter_vercel from '@sveltejs/adapter-vercel';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +16,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		// adapter: adapter(),
+		// adapter: adapter_static(),
+		adapter: adapter_vercel({runtime: 'nodejs20.x'}),
+
 		files: {
 			assets: '../static/'
 		},
@@ -26,7 +33,9 @@ const config = {
 
 				return config;
 			}
-		}
+		},
+		// outDir: '../build/v-svelte-ts-graphql',
+
 	}
 };
 
