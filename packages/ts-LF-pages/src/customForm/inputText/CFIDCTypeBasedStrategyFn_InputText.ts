fdb0c04c1b6ff606c40819087be093eeaef6f6e3
oxxxxxy <dawpbbgdfrphm212@outlook.com>
2025-04-06 05:01:55 +0300

    v-next-ts-graphql; fixing of problem with typechecking;

--- packages/ts-LF-pages/src/customForm/inputText/CFIDCTypeBasedStrategyFn_InputText.ts
+++ packages/ts-LF-pages/src/customForm/inputText/CFIDCTypeBasedStrategyFn_InputText.ts
@@ -11,12 +11,16 @@ import {
 
 
 
-export type InputText_ClassType_OneOf = typeof InputText_String
+export type InputText_ClassConstructorType_OneOf = typeof InputText_String
 	| typeof InputText_ExactString
 ;
 
+export type InputText_ClassType_OneOf = InputText_String
+	| InputText_ExactString
+;
+
 export type InputTextStrategyObj = {
-	[ prop in CFIDC_Types_InputText ]: InputText_ClassType_OneOf;
+	[ prop in CFIDC_Types_InputText ]: InputText_ClassConstructorType_OneOf;
 };
 
 export const InputTextStrategyObjData: InputTextStrategyObj = {
@@ -24,7 +28,7 @@ export const InputTextStrategyObjData: InputTextStrategyObj = {
 	'exact string': InputText_ExactString,
 };
 
-export const CFIDCTypeBasedStrategyFn_InputText = <T extends CFIDC_InputText_Base> (CFIDC_InputText: T): InputText_ClassType_OneOf => {
+export const CFIDCTypeBasedStrategyFn_InputText = <T extends CFIDC_InputText_Base> (CFIDC_InputText: T): InputText_ClassConstructorType_OneOf => {
 
 	const _class = InputTextStrategyObjData[CFIDC_InputText.type];
 
--- v-next-ts-graphql/src/components/next/customForm/InputText.tsx
+++ v-next-ts-graphql/src/components/next/customForm/InputText.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 
 import type { CFIDC_InputText_Base } from '@tsLF/pages';
@@ -15,65 +15,86 @@ import InputTextClearIcon from '@/components/next/customForm/icons/InputTextClea
 
 
 
-export let exit_value: QPC_InputText;
-export let entry_value: QPC_InputText;
+export default function InputText(
+	{
+		init_instanceOfInputText,
+		init_CFIDC_InputText,
+		init_cachedValue,
+		entry_value,
+		get_exitValue
+	}:{
+		entry_value: QPC_InputText;
+		init_cachedValue: QPC_InputText;
+		init_CFIDC_InputText: CFIDC_InputText_Base;
+		init_instanceOfInputText: InputText_ClassType_OneOf;
+		get_exitValue: (v: QPC_InputText) => {};
+	}
+){
+	const [placeholder, set_placeholder] = useState<string>('');
+	const [warning, set_warning] = useState<string>('');
+	const set_value = get_exitValue;
+
+
+
+// export let exit_value: QPC_InputText;
+// export let entry_value: QPC_InputText;
 	
-export let init_cachedValue: QPC_InputText;
-export let init_CFIDC_InputText: CFIDC_InputText_Base;
+// export let init_cachedValue: QPC_InputText;
+// export let init_CFIDC_InputText: CFIDC_InputText_Base;
 
-export let init_instanceOfInputText: InputText_ClassType_OneOf;
-
-
-let placeholder: string = '';
-let warning: string = '';
+// export let init_instanceOfInputText: InputText_ClassType_OneOf;
 
 
-const set_value = (v: QPC_InputText) => (exit_value = v);
-const set_warning = (w: string) => (warning = w);
-const set_placeholder = (p: string) => (placeholder = p);
+// let placeholder: string = '';
+// let warning: string = '';
+
+
+// const set_value = (v: QPC_InputText) => (exit_value = v);
+// const set_warning = (w: string) => (warning = w);
+// const set_placeholder = (p: string) => (placeholder = p);
 
 
 let inputText;
 	
-	if(init_instanceOfInputText){
-		init_instanceOfInputText.setBridgeToExternalScope(
-			{
-				set_warning,
-				set_placeholder,
-				set_value
-			}
-		)
-	
-		inputText = init_instanceOfInputText;
-
-		const value = inputText.getValue();
-
-		if(value.value){
-			init_cachedValue = value;
+if(init_instanceOfInputText){
+	init_instanceOfInputText.setBridgeToExternalScope(
+		{
+			set_warning,
+			set_placeholder,
+			set_value
 		}
-	} else {
-		if(!init_CFIDC_InputText){
-			throw new Error(`init_CFIDC_InputText must be passed.`);
-		}
+	)
+  
+	inputText = init_instanceOfInputText;
 
-		if(init_cachedValue){
-			if(init_cachedValue.param != init_CFIDC_InputText.name){
-				throw new Error(`Value of the init_cachedValue.param is not equal to init_CFIDC_InputText.name. Check passed args. ` + JSON.stringify(init_cachedValue));
-			}
-		}
+	const value = inputText.getValue();
 
-		const _class = CFIDCTypeBasedStrategyFn_InputText(init_CFIDC_InputText);
-
-		inputText = new _class(
-			{
-				initData: init_CFIDC_InputText,
-
-				set_value,
-				set_placeholder,
-				set_warning
-			}
-		);
+	if(value.value){
+		init_cachedValue = value;
 	}
+} else {
+	if(!init_CFIDC_InputText){
+		throw new Error(`init_CFIDC_InputText must be passed.`);
+	}
+
+	if(init_cachedValue){
+		if(init_cachedValue.param != init_CFIDC_InputText.name){
+			throw new Error(`Value of the init_cachedValue.param is not equal to init_CFIDC_InputText.name. Check passed args. ` + JSON.stringify(init_cachedValue));
+		}
+	}
+
+	const _class = CFIDCTypeBasedStrategyFn_InputText(init_CFIDC_InputText);
+
+	inputText = new _class(
+		{
+			initData: init_CFIDC_InputText,
+
+			set_value,
+			set_placeholder,
+			set_warning
+		}
+	);
+} 
 
 	const actionExecuterAfterMount = new U.ActionExecuterAfterCondition();
 	actionExecuterAfterMount.addAction(
@@ -116,40 +137,38 @@ let inputText;
 	});
 	
 
-export default function InputText(){
 
 	return(
-<div className="text-input-option d-flex w-100 fd-column"
->
-	<div className="fd-row d-flex w-100">
-	  <div className="text-input-box">
-	    <div className="text-input-underline bg-color--282828"></div>
-	    <input
-	      className="text-input bg-color--b6b6b6 color--282828"
-	      type="text"
-	      placeholder="{placeholder}"
-	      bind:value={_value}
-	    />
-	  </div>
-		<button
-		  className="ai-center jc-center text-input-clear fill--999999 bg-color--181a1b
-			{!_value ? 'button--empty' : 'button--has-some'}
-		  "
-		  disabled="{!_value}"
-		  on:click={clear}
-		>
-			<InputTextClearIcon />
-		</button>
-  </div>
-
-  {#if warning}
- 	 <div className="text-input-option d-flex w-100">
-			<span className="text-input-warning w-100">
-				{warning}
-			</span>
+		<div className="text-input-option d-flex w-100 fd-column">
+			<div className="fd-row d-flex w-100">
+			  <div className="text-input-box">
+			    <div className="text-input-underline bg-color--282828"></div>
+			    <input
+			      className="text-input bg-color--b6b6b6 color--282828"
+			      type="text"
+			      placeholder="{placeholder}"
+			      bind:value={_value}
+			    />
+			  </div>
+				<button
+				  className="ai-center jc-center text-input-clear fill--999999 bg-color--181a1b
+					{!_value ? 'button--empty' : 'button--has-some'}
+				  "
+				  disabled="{!_value}"
+				  on:click={clear}
+				>
+					<InputTextClearIcon />
+				</button>
+		  </div>
+		
+		  {#if warning}
+		 	 <div className="text-input-option d-flex w-100">
+					<span className="text-input-warning w-100">
+						{warning}
+					</span>
+				</div>
+			{/if}
+		
 		</div>
-	{/if}
-
-</div>
 	);
 }
