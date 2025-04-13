848d198d66f7e03b0b8653e8fd205a24f28efe28
oxxxxxy <dawpbbgdfrphm212@outlook.com>
2025-04-13 06:32:52 +0300

    v-next-ts-graphql; developing of /characters #3

--- v-next-ts-graphql/src/app/characters/page.tsx
+++ v-next-ts-graphql/src/app/characters/page.tsx
@@ -35,113 +35,237 @@ const pathName = API_CHARACTERS__PATH.name;
 export default function Characters(){
 
 
-	const pageTitle = capitalizeWord(API_CHARACTERS__PATH.name);
-	const	pathName = data.psl.route.id.slice(1);
+	// const pageTitle = capitalizeWord(API_CHARACTERS__PATH.name);
+	// const	pathName = data.psl.route.id.slice(1);
 
 
-	let CharactersSearch__exit_values: QueryParamCompatible_Base[] = [];
-	let pagination__exit_value: number | undefined;
+	// let CharactersSearch__exit_values: QueryParamCompatible_Base[] = [];
+	// let pagination__exit_value: number | undefined;
 
 
-	let CharactersSearch__update_values: QueryParamCompatible_Base[] = getQPCBaseListFromURL(new URL(data.psl.url));
-	let TileBoard_SearchUpdateValue: TileBoard_SearchValue | undefined;
-	let tiles: GT.CharacterPreviewFieldsFragment[] | NonTilesResultsForDrawingSearchPageTileBoard = 'LOADING';
+	// let CharactersSearch__update_values: QueryParamCompatible_Base[] = getQPCBaseListFromURL(new URL(data.psl.url));
+	// let TileBoard_SearchUpdateValue: TileBoard_SearchValue | undefined;
+	// let tiles: GT.CharacterPreviewFieldsFragment[] | NonTilesResultsForDrawingSearchPageTileBoard = 'LOADING';
 
 
-	const set_CharactersSearch__update_values = (v: QueryParamCompatible_Base[]) => (CharactersSearch__update_values = v);
-	const set_TileBoard_SearchValue = (v: TileBoard_SearchValue) => (TileBoard_SearchUpdateValue = v);
-	const set_tiles = (v: GT.CharacterPreviewFieldsFragment[] | NonTilesResultsForDrawingSearchPageTileBoard) => (tiles = v);
+	// const set_CharactersSearch__update_values = (v: QueryParamCompatible_Base[]) => (CharactersSearch__update_values = v);
+	// const set_TileBoard_SearchValue = (v: TileBoard_SearchValue) => (TileBoard_SearchUpdateValue = v);
+	// const set_tiles = (v: GT.CharacterPreviewFieldsFragment[] | NonTilesResultsForDrawingSearchPageTileBoard) => (tiles = v);
 
 
 
 
 
+	// const {
+	// 	handlePaginationSelection,
+	// 	handleCharactersSearchApply,
+	// 	actionExecuterAfterMount,
+	// 	searchPageManager
+	// } = initCharactersSearchPage(
+	// 	{
+	// 		pathName,
+	// 		pushStateFn: pushState,
+	// 		set_tiles,
+	// 		set_TileBoard_SearchValue,
+	// 		set_CharactersSearch__update_values,
+	// 		wUrql,
+	// 		wLocationChangeEventEmitter
+	// 	}
+	// );
+
+
+	// $:{
+	// 	tiles = tiles;
+	// }
+	// $:{
+	// 	handleCharactersSearchApply(CharactersSearch__exit_values);
+	// }
+	// $:{
+	// 	handlePaginationSelection(pagination__exit_value);
+	// }
+
+
+	// onMount(
+	// 	() => {
+	// 		actionExecuterAfterMount.setReady();
+			
+	// 		searchPageManager.init(CharactersSearch__update_values);
+	// 	}
+	// );
+
+
+	const { 
+		wUrql,
+		wLocationChangeEventEmitter
+	} = useGlobalContext();
+	const [
+		CharactersSearch__update_values,
+		set_CharactersSearch__update_values
+	] = useState<QueryParamCompatible_Base[]>([]); 
+	const [
+		TileBoard_SearchUpdateValue,
+		set_TileBoard_SearchValue
+	] = useState<TileBoard_SearchValue | undefined>(undefined);
+	const [
+		tiles,
+		set_tiles
+	] = useState<
+		GT.CharacterPreviewFieldsFragment[]
+		| NonTilesResultsForDrawingSearchPageTileBoard
+	>('LOADING');
+	const O = useRef(
+		initCharactersSearchPage(									              // prosti menya, gospod'... no ya greshen...
+			{                                                     //@ts-ignore
+				pathName,																						//@ts-ignore
+				pushStateFn: pushStateByLegacy,											//@ts-ignore
+				set_tiles,																					//@ts-ignore
+				set_TileBoard_SearchValue,													//@ts-ignore
+				set_CharactersSearch__update_values,								//@ts-ignore
+				wUrql,																							//@ts-ignore
+				wLocationChangeEventEmitter
+			}
+		)
+	);
 	const {
 		handlePaginationSelection,
 		handleCharactersSearchApply,
 		actionExecuterAfterMount,
 		searchPageManager
-	} = initCharactersSearchPage(
-		{
-			pathName,
-			pushStateFn: pushState,
-			set_tiles,
-			set_TileBoard_SearchValue,
-			set_CharactersSearch__update_values,
-			wUrql,
-			wLocationChangeEventEmitter
-		}
-	);
-
-
-	$:{
-		tiles = tiles;
+	} = O.current;
+	const get_CharactersSearch__exit_values = (v: QueryParamCompatible_Base[]) => {
+		// prosti menya, gospod'... no ya greshen...
+		//@ts-ignore
+		handleCharactersSearchApply(v);
 	}
-	$:{
-		handleCharactersSearchApply(CharactersSearch__exit_values);
+	const get_pagination__exit_value = (v: number | undefined) => {
+		// prosti menya, gospod'... no ya greshen...
+		//@ts-ignore
+		handlePaginationSelection(v);
 	}
-	$:{
-		handlePaginationSelection(pagination__exit_value);
+	
+
+	// prosti menya, gospod'... no ya greshen...
+	//@ts-ignore
+	if(!actionExecuterAfterMount.isReady()){
+		set_CharactersSearch__update_values(
+			getQPCBaseListFromURL(
+				new URL(window.location.href)
+			)
+		);
+		
+		// prosti menya, gospod'... no ya greshen...
+		//@ts-ignore
+		actionExecuterAfterMount.setReady();
+		
+		// prosti menya, gospod'... no ya greshen...
+		//@ts-ignore
+		searchPageManager.init(LocationsSearch__update_values);
 	}
 
 
-	onMount(
-		() => {
-			actionExecuterAfterMount.setReady();
+
+// <svelte:head>
+// 	<title>{ pageTitle } • { APP_NAME }</title>
+// 	<meta name="description" content="{ APP_NAME } { pageTitle }" />
+// </svelte:head>
+
+	return (
+		<>
+			<SearchItemNav pathName={pathName}>
+				<CharactersSearch
+					get_exit_values = {
+						get_CharactersSearch__exit_values
+					}
+					init_cachedValues = {
+						CharactersSearch__update_values
+					}
+					update_values = {
+						CharactersSearch__update_values
+					}
+				/>
+			</SearchItemNav>
+		
+			{
+				(
+					() => {
+				
+						if (tiles === 'ERR') {
+							return (
+								<TileBoard>
+									<p>Network Error. Try later or kill yourself. Thank you.</p>
+								</TileBoard>
+							);
+						} else if(tiles === 'LOADING') {
+							return (
+								<TileBoard>
+									<p>Loading...</p>
+								</TileBoard>
+							);
+						} else if (tiles === 'NOT FOUND'){
+							return (
+								<TileBoard>
+									<p>Nothing found.</p>
+								</TileBoard>
+							);
+						}else if (Array.isArray(tiles)){
+							let tileComponents = [];
+
+							for(let i = 0; i < tiles.length; i++){
+								tileComponents.push(
+									<CharacterTile data={tiles[i]} key={`${tiles[i].id} ${tiles[i].name}`} />
+								);
+							}
+
+							if(!TileBoard_SearchUpdateValue){
+								throw new Error('!TileBoard_SearchUpdateValue');
+							}
+
+							return (
+								<TileBoard_Search
+									update_value={
+										TileBoard_SearchUpdateValue
+									}
+									getPagination__exit_value={
+										get_pagination__exit_value
+									}
+								>
+									{tileComponents}
+								</TileBoard_Search>
+							);
+						}
 			
-			searchPageManager.init(CharactersSearch__update_values);
-		}
+					}
+				)()
+			}
+
+			
+			{/*#if tiles === 'ERR'}
+				<TileBoard>
+					<p>Network Error. Try later or kill yourself. Thank you.</p>
+				</TileBoard>
+			{:else if tiles === 'LOADING'}
+				<TileBoard>
+					<p>Loading...</p>
+				</TileBoard>
+			{:else if tiles === 'NOT FOUND'}
+				<TileBoard>
+					<p>Nothing found.</p>
+				</TileBoard>
+			{:else if Array.isArray(tiles)}
+				<TileBoard_Search
+					bind:update_value={
+						TileBoard_SearchUpdateValue
+					}
+					bind:pagination__exit_value={
+						pagination__exit_value
+					}
+				>
+				{#each tiles as tile }
+					<CharacterTile data={tile} />
+				{/each}
+				</TileBoard_Search>
+			{/if*/}
+
+		</>
 	);
-
-</script>
-
-
-
-
-<svelte:head>
-	<title>{ pageTitle } • { APP_NAME }</title>
-	<meta name="description" content="{ APP_NAME } { pageTitle }" />
-</svelte:head>
-
-
-<SearchItemNav {pathName}>
-	<CharactersSearch
-		bind:exit_values = {
-			CharactersSearch__exit_values
-		}
-		init_cachedValues = {
-			CharactersSearch__update_values
-		}
-		bind:update_values = {
-			CharactersSearch__update_values
-		}
-	/>
-</SearchItemNav>
-
-
-{#if tiles === 'ERR'}
-	<TileBoard>
-		<p>Network Error. Try later or kill yourself. Thank you.</p>
-	</TileBoard>
-{:else if tiles === 'LOADING'}
-	<TileBoard>
-		<p>Loading...</p>
-	</TileBoard>
-{:else if tiles === 'NOT FOUND'}
-	<TileBoard>
-		<p>Nothing found.</p>
-	</TileBoard>
-{:else if Array.isArray(tiles)}
-	<TileBoard_Search
-		bind:update_value={
-			TileBoard_SearchUpdateValue
-		}
-		bind:pagination__exit_value={
-			pagination__exit_value
-		}
-	>
-	{#each tiles as tile }
-		<CharacterTile data={tile} />
-	{/each}
-	</TileBoard_Search>
-{/if}
+}
---
+++ v-next-ts-graphql/src/components/next/routes/Characters/CharactersSearch.svelte
@@ -0,0 +1,224 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+
+	import {
+		API_CHARACTERS__PARAM__STATUS,
+		API_CHARACTERS__PARAM__GENDER,
+		API_CHARACTERS__PARAM__NAME,
+		API_CHARACTERS__PARAM__SPECIES,
+		API_CHARACTERS__PARAM__TYPE
+	} from '@tsCF/data';
+
+	import type { QueryParamCompatible_Base } from '@tsLF/forURLSP';
+
+	import { CustomFormHolder } from '@tsLF/pages';
+	import type { ArgumentsFor_CustomFormHolder } from '@tsLF/pages';
+
+	import { U } from '@tsL/utils';
+
+
+	import InputText from '$comps/svelte/customForm/InputText.svelte';
+	import SelectMenu from '$comps/svelte/customForm/SelectMenu.svelte';
+
+
+
+
+	export let init_cachedValues: QueryParamCompatible_Base[];
+	export let update_values: QueryParamCompatible_Base[];
+	export let exit_values: QueryParamCompatible_Base[];
+
+
+	let isValid: boolean = true;
+
+
+	const set_value = (v: QueryParamCompatible_Base[]) => (exit_values = v);
+	const set_applyActivity = (v: boolean) => (isValid = v);
+
+
+	const CFIDCList = [
+		API_CHARACTERS__PARAM__STATUS,
+		API_CHARACTERS__PARAM__GENDER,
+		API_CHARACTERS__PARAM__NAME,
+		API_CHARACTERS__PARAM__SPECIES,
+		API_CHARACTERS__PARAM__TYPE
+	];
+
+	const args: ArgumentsFor_CustomFormHolder = {
+		set_value,
+		set_applyActivity,
+
+		CFIDCList: CFIDCList
+	};
+
+	if(init_cachedValues){
+		args.cachedQPCValues = init_cachedValues;
+	}
+
+	const customFormHolder = new CustomFormHolder(args);
+	
+	const exitValueStore = CustomFormHolder.makeValueStore(CFIDCList);
+	let entryValueStore = CustomFormHolder.makeValueStore(CFIDCList);
+
+	const actionExecuterAfterMount = new U.ActionExecuterAfterCondition();
+	actionExecuterAfterMount.addAction(
+		() => {
+			const storeValue = CustomFormHolder.makeValueStore(CFIDCList);
+			
+			CustomFormHolder.setValuesToValueStore(storeValue, update_values);
+
+			entryValueStore = storeValue;
+
+		}
+	);
+
+
+	$: enabledDisabled = isValid ? {enabled: true} : {disabled: true};
+	$:{
+		customFormHolder.recieveExitValueStore(exitValueStore);
+
+		isValid = isValid;
+	}
+	$:{
+		actionExecuterAfterMount.exec();
+	
+		//svelte magic again. WTF?
+		update_values = update_values;
+		entryValueStore = entryValueStore;
+	}
+
+
+	onMount(() => {
+		actionExecuterAfterMount.setReady();
+		
+	})
+
+</script>
+
+<div class="margin-10 w-100 d-flex jc-center">
+  <div
+    class="search--width d-flex jc-space-between fd-column"
+    title="characters"
+  >
+
+		<InputText
+			bind:exit_value = {
+				exitValueStore[
+					API_CHARACTERS__PARAM__NAME
+					.name
+				]
+			}
+			bind:entry_value = {
+				entryValueStore[
+					API_CHARACTERS__PARAM__NAME
+					.name
+				]
+			}
+			init_instanceOfInputText = {
+				customFormHolder.getInstanceOfCFItemFor(
+					API_CHARACTERS__PARAM__NAME
+				)
+			}
+		/>
+		<InputText
+			bind:exit_value = {
+				exitValueStore[
+					API_CHARACTERS__PARAM__SPECIES
+					.name
+				]
+			}
+			bind:entry_value = {
+				entryValueStore[
+					API_CHARACTERS__PARAM__SPECIES
+					.name
+				]
+			}
+			init_instanceOfInputText = {
+				customFormHolder.getInstanceOfCFItemFor(
+					API_CHARACTERS__PARAM__SPECIES
+				)
+			}
+		/>
+		<InputText
+			bind:exit_value = {
+				exitValueStore[
+					API_CHARACTERS__PARAM__TYPE
+					.name
+				]
+			}
+			bind:entry_value = {
+				entryValueStore[
+					API_CHARACTERS__PARAM__TYPE
+					.name
+				]
+			}
+			init_instanceOfInputText = {
+				customFormHolder.getInstanceOfCFItemFor(
+					API_CHARACTERS__PARAM__TYPE
+				)
+			}
+		/>
+
+    <div class="filter-select-box d-flex jc-center ai-center">
+
+			<SelectMenu 
+				bind:exit_value = {
+					exitValueStore[
+						API_CHARACTERS__PARAM__STATUS
+						.name
+					]
+				}
+				bind:entry_value = {
+					entryValueStore[
+						API_CHARACTERS__PARAM__STATUS
+						.name
+					]
+				}
+				init_instanceOfSelectMenu = {
+					customFormHolder.getInstanceOfCFItemFor(
+						API_CHARACTERS__PARAM__STATUS
+					)
+				}
+			/>
+
+      <div
+        class="show-if-desktop-size"
+        style="margin-left: 5px; height: 100%"
+      ></div>
+
+			<SelectMenu 
+				bind:exit_value = {
+					exitValueStore[
+						API_CHARACTERS__PARAM__GENDER
+						.name
+					]
+				}
+				bind:entry_value = {
+					entryValueStore[
+						API_CHARACTERS__PARAM__GENDER
+						.name
+					]
+				}
+				init_instanceOfSelectMenu = {
+					customFormHolder.getInstanceOfCFItemFor(
+						API_CHARACTERS__PARAM__GENDER
+					)
+				}
+			/>
+
+    </div>
+
+    <button
+      class="
+				filter-button color--b6b6b6 bg-color--181a1b tt-uppercase 
+				{ isValid ? 'button--has-some': 'button--empty'}
+			"
+
+			{...enabledDisabled}
+
+		  on:click={() => customFormHolder.apply()}
+    >
+      Apply
+    </button>
+  </div>
+</div>
