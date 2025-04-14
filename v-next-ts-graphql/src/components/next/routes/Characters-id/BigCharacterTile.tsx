4f3b2a9651a5fef3716edf4b48b0b6e031cbd40b
oxxxxxy <dawpbbgdfrphm212@outlook.com>
2025-04-14 12:53:48 +0300

    v-next-ts-graphql; developing of BigCharacterTile.tsx #2, it was done;

--- v-next-ts-graphql/src/components/next/routes/Characters-id/BigCharacterTile.tsx
+++ v-next-ts-graphql/src/components/next/routes/Characters-id/BigCharacterTile.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import Link from 'next/link';
 
 
 import type { GT } from '@tsC/api-graphql-to-ex';
@@ -13,282 +13,312 @@ import EpisodeTile from '@/components/next/tileBoard/tiles/EpisodeTile';
 
 
 
-export let data: GT.CharacterFieldsFragment;
+export default function BigCharacterTile(
+	{
+		data
+	}:{
+		data: GT.CharacterFieldsFragment;
+	}
+){
+	const cssClass_CharacterStatusIcon = getCssClass_CharacterStatusIcon(data.status);
 
-
-const cssClass_CharacterStatusIcon = getCssClass_CharacterStatusIcon(data.status);
-
-
-
-
-
-<div
-	class="
-		bg-color--181a1b
-		d-flex
-		big-tile-box
-	"
-> 
-	<div
-		class="
-			big-tile-img-box
-		"
-	>
-		<img class="tile-img-item" alt="{data.name}" src="{data.image}">
-	</div>
-	<div
-		class="
-			big-tile-info-box
-			d-flex
-			fd-column
-		"
-	>
-    <span
-			class="
-				big-tile-h
-				tile-line
-			"
-    >
-			Character card
-    </span>
-		<span
-			class="
-				tile-line
-			"
-			style="
-				border-bottom: solid 3px var(--colorPalette-b6b6b6);
-				width: 100%;
-			"
-		></span>
-    <span
-			class="
-				tile-line
-				font-weight--normal
-			"
-    >
-			Name:
-	    <span
-				class="
-					color--f5f5f5
+	return (
+		<>
+			<div
+				className="
+					bg-color--181a1b
+					d-flex
+					big-tile-box
 				"
-	    >
-				{ data.name }
-	    </span>
-    </span>
-
-    <span 
-			class="
-				tile-line
-				font-weight--normal
-			"
-		>
-			Gender:
-	    <span 
-				class="
-					color--f5f5f5
-				"
-			>
-				{ data.gender}
-			</span>
-    </span>
-
-		{#if data.type}
-	    <span 
-				class="
-					tile-line
-					font-weight--normal
-				"
-			>
-				Type:
-		    <span 
-					class="
-						color--f5f5f5
+			> 
+				<div
+					className="
+						big-tile-img-box
 					"
 				>
-					{ data.type }
-				</span>
-	    </span>
-    {/if}
-
-    <span
-			class="
-				tile-line
-				font-weight--normal
-				character-status
-				ai-center
-			"
-    >
-			Status:
-	    <span 
-				class="
-					character-status
-					color--f5f5f5
-				"
-			>
-				<span 
-					class="
-						character-status__big-icon
-						{cssClass_CharacterStatusIcon}
-					"
-				></span>
-				{ data.status }
-			</span>
-    </span>
-
-    <span 
-			class="
-				tile-line
-				font-weight--normal
-			"
-		>
-			Spieces:
-	    <span 
-				class="
-					color--f5f5f5
-				"
-			>
-				{ data.species }
-			</span>
-    </span>
-
-		{#if data.origin.id}
-			<a
-				class="
-					no-underline
-					tile-line
-						font-weight--normal
-				"
-		    href="{ '/locations/' + data.origin.id }"
-		    rel="noopener noreferrer"
-			>
-		 	  <span 
-					class="
-						tile-line
-						font-weight--normal
-					"
-				>Origin location:</span>
-		 	  <span
-					class="
-						color--f5f5f5
-						tile-line
-						underline
-					"
-			   >
-					{ data.origin.name }
-		 	  </span>
-			</a>
-		{:else}
-			<a
-				class="
-					no-underline
-					tile-line
-					font-weight--normal
-				"
-			>
-		 	  <span 
-					class="
-						tile-line
-						font-weight--normal
-					"
-				>Origin location:</span>
-		 	  <span
-					class="
-						color--f5f5f5
-						tile-line
-						underline
-					"
-			   >
-					{ data.origin.name }
-		 	  </span>
-			</a>
-		{/if}
-
-		{#if data.location.id}
-			<a
-				class="
-					no-underline
-					tile-line
-						font-weight--normal
-				"
-		    href="{ '/locations/' + data.location.id }"
-		    rel="noopener noreferrer"
-			>
- 		   <span 
-					class="
-						tile-line
-						font-weight--normal
+					<img className="tile-img-item" alt={data.name} src={data.image}/>
+				</div>
+				<div
+					className="
+						big-tile-info-box
+						d-flex
+						fd-column
 					"
 				>
-					Last known location:
-				</span>
-	 		  <span
-					class="
-						color--f5f5f5
-						tile-line
-						underline
-					"
-			   >
-					{ data.location.name }
-		    </span>
-			</a>
-		{:else}
-			<a
-				class="
-					no-underline
-					tile-line
-						font-weight--normal
+			    <span
+						className="
+							big-tile-h
+							tile-line
+						"
+			    >
+						Character card
+			    </span>
+					<span
+						className="
+							tile-line
+						"
+						style={{
+							borderBottom: 'solid 3px var(--colorPalette-b6b6b6)',
+							width: '100%'
+						}}
+					></span>
+			    <span
+						className="
+							tile-line
+							font-weight--normal
+						"
+			    >
+						Name:
+				    <span
+							className="
+								color--f5f5f5
+							"
+				    >
+							{ data.name }
+				    </span>
+			    </span>
+			
+			    <span 
+						className="
+							tile-line
+							font-weight--normal
+						"
+					>
+						Gender:
+				    <span 
+							className="
+								color--f5f5f5
+							"
+						>
+							{ data.gender}
+						</span>
+			    </span>
+			
+					{
+						(() => {
+							if(data.type){
+								return (
+									<span 
+										className="
+											tile-line
+											font-weight--normal
+										"
+									>
+										Type:
+					 			   <span 
+											className="
+												color--f5f5f5
+											"
+										>
+											{ data.type }
+										</span>
+				  			  </span>
+								);
+							}
+						})()
+			    }
+			
+			    <span
+						className="
+							tile-line
+							font-weight--normal
+							character-status
+							ai-center
+						"
+			    >
+						Status:
+				    <span 
+							className="
+								character-status
+								color--f5f5f5
+							"
+						>
+							<span 
+								className={`
+									character-status__big-icon
+									${cssClass_CharacterStatusIcon}
+								`}
+							></span>
+							{ data.status }
+						</span>
+			    </span>
+			
+			    <span 
+						className="
+							tile-line
+							font-weight--normal
+						"
+					>
+						Spieces:
+				    <span 
+							className="
+								color--f5f5f5
+							"
+						>
+							{ data.species }
+						</span>
+			    </span>
+			
+					{
+						(() => {
+							if (data.origin.id){
+								return (
+									<Link
+										className="
+											no-underline
+											tile-line
+												font-weight--normal
+										"
+								    href={ '/locations/' + data.origin.id }
+								    rel="noopener noreferrer"
+									>
+								 	  <span 
+											className="
+												tile-line
+												font-weight--normal
+											"
+										>Origin location:</span>
+								 	  <span
+											className="
+												color--f5f5f5
+												tile-line
+												underline
+											"
+									   >
+											{ data.origin.name }
+								 	  </span>
+									</Link>
+								);
+							}else{
+								return (
+									<a
+										className="
+											no-underline
+											tile-line
+											font-weight--normal
+										"
+									>
+								 	  <span 
+											className="
+												tile-line
+												font-weight--normal
+											"
+										>Origin location:</span>
+								 	  <span
+											className="
+												color--f5f5f5
+												tile-line
+												underline
+											"
+									   >
+											{ data.origin.name }
+								 	  </span>
+									</a>
+								);
+							}
+						})()
+					}
+			
+					{
+						(() => {
+							if (data.location.id){
+								return (
+									<Link
+										className="
+											no-underline
+											tile-line
+												font-weight--normal
+										"
+					 		 	  href={ '/locations/' + data.location.id }
+					 		 	  rel="noopener noreferrer"
+									>
+			 		 		 		 <span 
+												className="
+													tile-line
+													font-weight--normal
+												"
+											>
+												Last known location:
+											</span>
+				 						  <span
+												className="
+													color--f5f5f5
+													tile-line
+													underline
+												"
+										   >
+												{ data.location.name }
+					 		 		  </span>
+									</Link>
+								)
+							}else{
+								return (
+									<a
+										className="
+											no-underline
+											tile-line
+												font-weight--normal
+										"
+					 		 	  rel="noopener noreferrer"
+									>
+										<span 
+												className="
+													tile-line
+													font-weight--normal
+												"
+											>
+												Last known location:
+											</span>
+				 						  <span
+												className="
+													color--f5f5f5
+													tile-line
+													underline
+												"
+										   >
+												{ data.location.name }
+					 		 		  </span>
+									</a>
+								)
+							}
+						})()
+					}
+			
+				</div>
+			</div>
+			<div
+				className="
+					big-result-board
+					w-100
 				"
-		    rel="noopener noreferrer"
+				style={{
+					marginTop: '10px',
+					backgroundColor: 'var(--colorPalette-181a1b)'
+				}}
 			>
- 		   <span 
-					class="
-						tile-line
-						font-weight--normal
+			
+				<div
+					className="
+						d-flex
+						w-100
+						jc-center
+						bg-color--181a1b
 					"
+					style={{
+						marginBottom: '10px'
+					}}
 				>
-					Last known location:
-				</span>
-	 		  <span
-					class="
-						color--f5f5f5
-						tile-line
-						underline
-					"
-			   >
-					{ data.location.name }
-		    </span>
-			</a>
-		{/if}
-
-	</div>
-</div>
-<div
-	class="
-		big-result-board
-		w-100
-	"
-	style="
-		margin-top: 10px;
-		background-color: var(--colorPalette-181a1b);
-	" 
->
-
-	<div
-		class="
-			d-flex
-			w-100
-			jc-center
-			bg-color--181a1b
-		"
-		style="
-			margin-bottom: 10px;
-		"
-	>
-		List of episodes in which this character appeared.
-	</div>
-
-	{#each data.episode as ep }
-		<EpisodeTile data={ep} />
-	{/each}
-
-</div>
+					List of episodes in which this character appeared.
+				</div>
+			
+					
+				{
+					data.episode.map((ep, i) => 
+						<EpisodeTile data={ep} key={i}/>
+					)
+				}
+			
+			</div>
+		</>
+	);
+}
