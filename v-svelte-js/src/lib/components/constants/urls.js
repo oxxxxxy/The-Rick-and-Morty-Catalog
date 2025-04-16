7ae92a39528506a7e533591a0aeebdcf7c6b72ee
oxxxxxy <dawpbbgdfrphm212@outlook.com>
2025-04-16 21:45:30 +0300

    cleaning up of the shit

--- v-svelte-js/src/lib/components/constants/urls.js
+++
@@ -1,4 +0,0 @@
-export { URL__STATUS_RICKANDMORTYAPI } from '$lib/global-components-exit.js';
-
-
-export const URL__APP_VER_GITHUB_REPO = 'https://github.com/oxxxxxy/The-Rick-and-Morty-Catalog/tree/main/v-svelte-js-axios';
--- v-svelte-js/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="The Rick and Morty Catalog" />
-</svelte:head>
-
