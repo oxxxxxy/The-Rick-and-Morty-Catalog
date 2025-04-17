12cf93ff17c038a183b3d4e47c7ac3f098a56f18
oxxxxxy <dawpbbgdfrphm212@outlook.com>
2025-04-17 20:21:06 +0300

    linting try, building issues fixing try #4, mi pobedili https://s.pikabu.ru/post_img/2013/08/26/5/1377493471_618773743.jpg

--- v-next-ts-graphql/next.config.mjs
+++ v-next-ts-graphql/next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	/* eslint:{
-		ignoreDuringBuilds: true,
-	},
 	typescript: {
-		ignoreBuildErrors: true
-	} */
+    ignoreBuildErrors: (error) => {
+      const ignoredLibs = [
+        /packages\/ts\-/,
+        /node_modules\/@problematic\-lib/
+      ];
+      return ignoredLibs.some(pattern => pattern.test(error.file));
+    }
+	}
+	
 };
 
 export default nextConfig;
