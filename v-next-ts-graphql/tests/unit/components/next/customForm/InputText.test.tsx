83dfe86e803c4a9904a40fe752d44c11c856087a
oxxxxxy <dawpbbgdfrphm212@outlook.com>
2025-04-22 20:47:26 +0300

    v-next-ts-graphql; writing unit tests on vitest; InputText.test.tsx #2 was done

--- v-next-ts-graphql/tests/unit/components/next/customForm/InputText.test.tsx
+++ v-next-ts-graphql/tests/unit/components/next/customForm/InputText.test.tsx
@@ -1,11 +1,10 @@
-import { 
-	render,
-	fireEvent
-} from '@testing-library/react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import { 
 	expect,
 	describe,
-	it
+	it,
+	vi
 } from 'vitest';
 
 
@@ -18,7 +17,10 @@ import type {
 	QPC_InputText
 } from '@tsLF/forURLSP';
 
-import { CustomFormHolder } from '@tsLF/pages';
+import { 
+	CustomFormHolder,
+	CFIDCTypeBasedStrategyFn_All 
+} from '@tsLF/pages';
 import type { ValueStore } from '@tsLF/pages';
 
 
@@ -27,19 +29,30 @@ import InputText from '@/components/next/customForm/InputText';
 
 
 
-describe(`<InputText /> ; component; next/customForm/InputText.tsx;`, () => {
-	const entryValueStore: ValueStore = CustomFormHolder.makeValueStore(API_EPISODES__PARAM_LIST);
+const entryValueStore: ValueStore = CustomFormHolder.makeValueStore(API_EPISODES__PARAM_LIST); 
 
-	const get_exitValue = (v: QPC_InputText) => {};
+const get_exitValue = (v: QPC_InputText) => {}; 
 
-	const props = {
-		entry_value: entryValueStore[
-				API_EPISODES__PARAM__EPISODE.name
-			],
-		get_exitValue,
-		init_CFIDC_InputText: API_EPISODES__PARAM__EPISODE
-	}
+const props = { 
+	entry_value: entryValueStore[ 
+			API_EPISODES__PARAM__EPISODE.name 
+		], 
+	get_exitValue, 
+	init_CFIDC_InputText: API_EPISODES__PARAM__EPISODE 
+} 
 
+
+describe(`<InputText /> ; next/customForm/InputText.tsx;`, () => {
+	it(`gets entry_value, get_exitValue and throws error.`, () => {
+		const _props = {...props};
+		delete _props.init_CFIDC_InputText;
+		
+		const check = () => {
+			render(<InputText {..._props} />);
+		}
+
+		expect(check).toThrowError();
+	})
 
 	it(`gets entry_value, get_exitValue and init_CFIDC_InputText and render it`, () => {
 		const check = () => {
@@ -50,16 +63,32 @@ describe(`<InputText /> ; component; next/customForm/InputText.tsx;`, () => {
 		
 		expect(check).not.toThrowError();
 	})
-
-	it(`gets entry_value, get_exitValue and throws error.`, () => {
+	
+	it(`gets entry_value, get_exitValue, init_CFIDC_InputText, incorrect init_cachedValue and throws error.`, () => {
 		const _props = {...props};
-		delete _props.init_CFIDC_InputText;
-		
-		const check = () => {
-			render(<InputText {..._props} />);
-		}
+		_props.init_cachedValue = { param: "not episode", value: "doesn't matter" };
 
+		const check = () => {
+			const component = render(<InputText {..._props} />);
+		}
+		
 		expect(check).toThrowError();
+	})
+	
+	it(`gets entry_value, get_exitValue and init_instanceOfInputText and render it`, () => {
+		const CFItem = CFIDCTypeBasedStrategyFn_All(API_EPISODES__PARAM__EPISODE);
+		const init_instanceOfInputText = new CFItem({initData: API_EPISODES__PARAM__EPISODE});
+	
+		const _props = {...props, init_instanceOfInputText};
+		delete _props.init_CFIDC_InputText;
+
+		const check = () => {
+			const component = render(<InputText {..._props} />);
+			
+			component.unmount();
+		}
+		
+		expect(check).not.toThrowError();
 	})
 
 
@@ -75,14 +104,76 @@ describe(`<InputText /> ; component; next/customForm/InputText.tsx;`, () => {
 		component.unmount();
 	})	
 
+	it(`gets entry_value, get_exitValue, init_CFIDC_InputText, init_cachedValue and renders it with value.`, () => {
+		const _props = {...props};
+		_props.init_cachedValue = { param: "episode", value: "matters" };
+		_props.get_exitValue = v => {
+			expect(vi.fn()).not.toHaveBeenCalled();
+		}
+
+		const check = () => {
+			const component = render(<InputText {..._props} />);
+
+			const input = component.container.querySelector('input.text-input');
+
+			expect(input.value).toEqual('matters');
+
+			component.unmount();
+		}
+
+		expect(check).not.toThrowError();
+	})
+
+	it('checks returning of the value via get_exitValue while user is typing', async () => {
+		const userInputText = 'S02E03';
+		const _props = {...props};
+		_props.get_exitValue = v => {
+			expect(!!userInputText.match(v.value)).toBe(true);
+		}
 
 
-// 		const input = component.container.querySelector('input.text-input');
-// console.log(component)
-		// console.log(input.placeholder);
-	// console.log(API_EPISODES__PARAM__EPISODE)
-	// 	screen.getByPlaceholderText(API_EPISODES__PARAM__EPISODE.hint)
+		const component = render(<InputText {..._props} />);
 
-		// component.unmount();
+		const input = component.container.querySelector('input.text-input');
+
+		await userEvent.type(input, userInputText);
+
+
+		component.unmount();
+	})
 	
+	it('throws warning if user input value is incorrect, then clears it and checks', async () => {
+		let testStep = 'throws warning';
+
+		const userInputText = 'NOTS02E03';
+		const _props = {...props};
+		_props.get_exitValue = v => {
+			if(testStep === 'throws warning'){
+				expect(!!v.warning).toBe(true);
+			}else if(testStep === 'clears it and checks'){
+				expect(!!v.value).toBe(false);
+			}
+		}
+
+
+		const component = render(<InputText {..._props} />);
+		const input = component.container.querySelector('input.text-input');
+
+		expect(!!component.container.querySelector('span.text-input-warning')).toBe(false);
+		expect(!!component.container.querySelector('button').disabled).toBe(true);
+
+		await userEvent.type(input, userInputText);
+
+		expect(!!component.container.querySelector('span.text-input-warning')).toBe(true);
+		expect(!!component.container.querySelector('button').disabled).toBe(false);
+		
+		testStep = 'clears it and checks';
+		await userEvent.click(component.container.querySelector('button'));
+
+		expect(!!component.container.querySelector('span.text-input-warning')).toBe(false);
+		expect(!!component.container.querySelector('button').disabled).toBe(true);
+
+
+		component.unmount();
+	})
 });
