f29283dc78eac99f03200ecad371f09665cf2488
oxxxxxy <dawpbbgdfrphm212@outlook.com>
2025-05-02 21:42:31 +0300

    v-next-ts-graphql; writing e2e tests; playwright; localhost:port/

---
+++ v-next-ts-graphql/tests/e2e-playwright/app/route-home/navigation from this point.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+
+
+
+test.describe(`navigation from localhost:port/`, () => {
+	test(``, () => {
+		
+	})
+
+
+	/*
+go to git repo via logo, poster
+go to rickandmortyapi status via header
+go to rickandmorty via poster
+
+
+		 
+	 */
+});
