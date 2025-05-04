3051bf222f7a2dfef5503116769db1a2fbdfbbed
oxxxxxy <dawpbbgdfrphm212@outlook.com>
2025-05-04 23:08:52 +0300

    v-next-ts-graphql; writing e2e tests; playwright; localhost:port/ #5, tak huevo mne chota

--- v-next-ts-graphql/tests/e2e-playwright/app/route-home/navigation from this point.spec.ts
+++ v-next-ts-graphql/tests/e2e-playwright/app/route-home/navigation from this point.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page, Locator } from '@playwright/test';
 
 
 
@@ -21,8 +21,62 @@ test(`navigation from localhost:port/`, async ({page}) => {
 	).toBe(undefined);
 
 
-	await page.
+	await page.waitForFunction(
+		() => {
+			const tiles = [...document.querySelector('div.result-board').children];
+			
+			if(
+				tiles.every(e => [...e.classList].includes('tile-box'))
+			){
+				return true;
+			}
+		},
+		{ timeout: 5000 }
+	);
 
-		suka ves' den' spal... ne beyte.
+	const getTile = async (page: Page, tileType: string, r): Locator | undefined => {
+		let tile: Locator | undefined = undefined;
+	console.log('getTile')
+
+		for(const loc of await page.locator('div.tile-box').all()){
+			console.log('adf')
+			if(!tile){
+				const bool = await loc.evaluate((e, tileType) => {
+						if(e.getAttributeNames().includes(tileType)){
+							return true;
+						}
+					},
+					tileType
+				)
+console.log(bool, 'bool')	
+				if(bool){
+					tile = loc
+				}
+			}
+		}
+
+		console.log('end of getTile')
+		// r(tile);
+		return tile;
+	}
+
+	let timesOfFoundTilesInRandom = 0;
+
+
+	// const charTile = await new Promise(r => {getTile(page, 'data-character-tile', r)});
+	const charTile = await getTile(page, 'data-character-tile');
+
+	if(charTile){
+		timesOfFoundTilesInRandom++;
+		
+		// await charTile.locator('div.tile-img-box').click();
+
+	}
+
+	console.log(charTile)
+
+	if(timesOfFoundTilesInRandom < 3){
+		throw new Error(`timesOfFoundTilesInRandom < 3. very bad scenario. i guess we have changes in NineRandomTiles component which break test. Execute it again, until 2 times.`)
+	}
 
 });
